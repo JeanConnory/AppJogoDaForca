d25b601177297a09a07107579647082acbf198b7
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-07-06 15:44:23 -0300

    Jogo da forca concluido

---
+++ Models/Word.cs
@@ -0,0 +1,14 @@
+﻿namespace AppJogoForca.Models;
+
+public class Word
+{
+    public string Tips { get; set; } = string.Empty;
+
+    public string Text { get; set; } = string.Empty;
+
+    public Word(string tips, string text)
+    {
+        this.Tips = tips;
+        this.Text = text;
+    }
+}
---
+++ Repositories/WordRepositories.cs
@@ -0,0 +1,25 @@
+﻿using AppJogoForca.Models;
+
+namespace AppJogoForca.Repositories;
+
+public class WordRepositories
+{
+    private List<Word> _words;
+
+    public WordRepositories()
+    {
+        _words = new List<Word>();
+        _words.Add(new Word("Nome", "Maria".ToUpper()));
+        _words.Add(new Word("Vegetal", "Cenoura".ToUpper()));
+        _words.Add(new Word("Fruta", "Abacate".ToUpper()));
+        _words.Add(new Word("País", "Brasil".ToUpper()));
+        _words.Add(new Word("Tempero", "Nordestino".ToUpper()));  
+    }
+
+    public Word GetRandomWord()
+    {
+        Random random = new Random();
+        var number = random.Next(0, _words.Count);
+        return _words[number];
+    }
+}
